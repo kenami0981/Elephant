c5a29cb43b3f8d0c4585b70dbb66d251a0238c63
kenami0981 <amelia.janek.aj@gmail.com>
2022-05-24 22:28:03 +0200

    Nav & about-us page

---
+++ projects/founders/src/lib/adapters/primary/components/founders.component.ts
@@ -0,0 +1,5 @@
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+
+@Component({ selector: 'lib-founders', templateUrl: './founders.component.html', encapsulation: ViewEncapsulation.None, changeDetection: ChangeDetectionStrategy.OnPush })
+export class FoundersComponent {
+}
