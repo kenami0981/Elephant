80a976f103a0f34f8cb7de2b228a582c9914b320
AniaTofik <anna.bodal@op.pl>
2022-05-27 12:57:07 +0200

    added promotion section to Firebase

--- projects/promo/src/lib/adapters/primary/ui/promotion.component.ts
+++ projects/promo/src/lib/adapters/primary/ui/promotion.component.ts
@@ -1,5 +1,17 @@
-import { Component, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Inject, ViewEncapsulation } from '@angular/core';
+import { Observable } from 'rxjs';
+import { PromotionDTO } from '../../../application/ports/secondary/dto/promotion.dto';
+import { GETS_ALL_PROMOTION_DTO, GetsAllPromotionDtoPort } from '../../../application/ports/secondary/dto/gets-all-promotion.dto-port';
 
-@Component({ selector: 'lib-promotion', templateUrl: './promotion.component.html', encapsulation: ViewEncapsulation.None, changeDetection: ChangeDetectionStrategy.OnPush })
+@Component({
+  selector: 'lib-promotion',
+  templateUrl: './promotion.component.html',
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
 export class PromotionComponent {
+  promotions$: Observable<PromotionDTO[]> = this._getsAllPromotionDto.getAll();
+
+  constructor(@Inject(GETS_ALL_PROMOTION_DTO) private _getsAllPromotionDto: GetsAllPromotionDtoPort) {
+  }
 }
