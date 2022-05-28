5049e6c88d6b45e85acce9fdcb9c28063857a041
AniaTofik <anna.bodal@op.pl>
2022-05-28 12:10:38 +0200

    fixed promotion.ts and app.module.ts

--- projects/promo/src/lib/adapters/primary/ui/promotion.component.ts
+++ projects/promo/src/lib/adapters/primary/ui/promotion.component.ts
@@ -1,27 +1,18 @@
-
 import { ChangeDetectionStrategy, Component, Inject, ViewEncapsulation } from '@angular/core';
 import { Observable } from 'rxjs';
 import { PromotionDTO } from '../../../application/ports/secondary/dto/promotion.dto';
 import { GETS_ALL_PROMOTION_DTO, GetsAllPromotionDtoPort } from '../../../application/ports/secondary/dto/gets-all-promotion.dto-port';
 
-import {
-  Component,
-  ViewEncapsulation,
-  ChangeDetectionStrategy,
-} from '@angular/core';
-
-
 @Component({
-  selector: 'lib-promotion',
-  templateUrl: './promotion.component.html',
-  encapsulation: ViewEncapsulation.None,
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  selector: 'lib-promotion',
+  templateUrl: './promotion.component.html',
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-
 export class PromotionComponent {
-  promotions$: Observable<PromotionDTO[]> = this._getsAllPromotionDto.getAll();
+  promotions$: Observable<PromotionDTO[]> = this._getsAllPromotionDto.getAll();
 
-  constructor(@Inject(GETS_ALL_PROMOTION_DTO) private _getsAllPromotionDto: GetsAllPromotionDtoPort) {
-  }
+  constructor(@Inject(GETS_ALL_PROMOTION_DTO) private _getsAllPromotionDto: GetsAllPromotionDtoPort) {
+  }
 }
 
