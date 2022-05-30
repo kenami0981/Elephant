649fc44826deb4da15a1280d03c71efab1f47463
AniaTofik <anna.bodal@op.pl>
2022-05-30 13:11:45 +0200

    fixed file tsconfig

--- projects/products/src/index.ts
+++ projects/products/src/index.ts
@@ -1,2 +1,3 @@
 export * from './lib/adapters/primary/ui/products.component-module';
 export * from './lib/adapters/secondary/infrastructure/firebase-products.service-module';
+export * from './lib/adapters/primary/components/products.component-module';
