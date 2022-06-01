2315c33fd75c004f45d86e17a822bc9fcad31e19
Adam <92727986+AWieczorek00@users.noreply.github.com>
2022-06-01 18:13:23 +0200

    feat/add href to button buy now, shop now and etc (#24)
    * feat/add href to button buy now, shop now and etc
    
    * feat/add href to button buy now, shop now and etc
    
    * reaplace href with routerlink in buttons

--- projects/hero/src/lib/adapters/primary/ui/hero.component-module.ts
+++ projects/hero/src/lib/adapters/primary/ui/hero.component-module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeroComponent } from './hero.component';
+import { RouterModule } from '@angular/router';
 
-@NgModule({ imports: [CommonModule],
+
+@NgModule({ imports: [CommonModule, [RouterModule]],
   	declarations: [HeroComponent],
   	providers: [],
   	exports: [HeroComponent] })
