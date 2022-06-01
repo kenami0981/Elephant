ad5b6fdf14215fa8304c67db72bb7d2a9920360d
AWieczorek00 <awieczorek0000@gmail.com>
2022-06-01 12:55:29 +0200

    reaplace href with routerlink in buttons

--- projects/founders/src/lib/adapters/primary/components/founders.component-module.ts
+++ projects/founders/src/lib/adapters/primary/components/founders.component-module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FoundersComponent } from './founders.component';
+import { RouterModule } from '@angular/router';
 
-@NgModule({ imports: [CommonModule],
+
+@NgModule({ imports: [CommonModule, RouterModule],
   	declarations: [FoundersComponent],
   	providers: [],
   	exports: [FoundersComponent] })
--- projects/selling/src/lib/adapters/primary/ui/best-selling.component-module.ts
+++ projects/selling/src/lib/adapters/primary/ui/best-selling.component-module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BestSellingComponent } from './best-selling.component';
+import { RouterModule } from '@angular/router';
 
-@NgModule({ imports: [CommonModule],
+
+@NgModule({ imports: [CommonModule ,[RouterModule]],
   	declarations: [BestSellingComponent],
   	providers: [],
   	exports: [BestSellingComponent] })
