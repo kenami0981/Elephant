7cf2ae0945eba9e72fb605aa55a909548ee15bf7
KarolinaUser <100318220+KarolinaUser@users.noreply.github.com>
2022-06-04 17:09:34 +0200

    feat: contact-alert (#37)

--- projects/contact/src/lib/adapters/primary/components/form.component.ts
+++ projects/contact/src/lib/adapters/primary/components/form.component.ts
@@ -2,6 +2,7 @@ import { Message } from '@angular/compiler/src/i18n/i18n_ast';
 import { ChangeDetectionStrategy, Component, Inject, ViewEncapsulation } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { ADDS_CONTACT_DTO, AddsContactDtoPort } from '../../../application/ports/secondary/dto/adds-contact.dto-port';
+import Swal from 'sweetalert2' 
 
 @Component({ 
     selector: 'lib-form', 
@@ -22,5 +23,10 @@ export class FormComponent {
 onContactSubmited(contact: FormGroup): void {
   this._addsContactDto.add(contact.getRawValue());
   contact.reset();
+  Swal.fire(
+    'Message sent!',
+    'Well contact you soon.',
+    'success'
+  )
 }
 }
