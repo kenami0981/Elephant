0a49b7ce4af0a043417a682595179bc3d317ab6e
AniaTofik <anna.bodal@op.pl>
2022-06-07 10:10:04 +0200

    created buttons to increase and decrease the product in the cart

--- projects/cart/src/lib/adapters/primary/components/cart.component.ts
+++ projects/cart/src/lib/adapters/primary/components/cart.component.ts
@@ -1,5 +1,27 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+  ViewEncapsulation,
+} from '@angular/core';
 
-@Component({ selector: 'lib-cart', templateUrl: './cart.component.html', encapsulation: ViewEncapsulation.None, changeDetection: ChangeDetectionStrategy.OnPush })
+@Component({
+  selector: 'lib-cart',
+  templateUrl: './cart.component.html',
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
 export class CartComponent {
+  count: number = 0;
+
+  incCounter(type: string) {
+    if (type === 'add') {
+      this.count++;
+    }
+  }
+  decrCounter(type: string) {
+    if (type === 'minus') {
+      this.count = this.count - 1 > 0 ? this.count - 1 : 0;
+    }
+  }
 }
